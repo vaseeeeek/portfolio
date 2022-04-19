63185d8c97e4d5e6e79f5c16f2f90601e4024615
vaseeeeek <vaseeeeek53@gmail.com>
2022-04-19 19:22:30 +0300

    1

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,6 +1,37 @@
 import Vue from "vue";
 
+const tools = {
+  template: "#slider-tools",
+};
+
+const thumbs = {
+  template: "#slider-thumbs",
+};
+
+const buttons = {
+  template: "#slider-buttons",
+};
+
+const info = {
+  template: "#slider-description",
+  components: {
+    tools
+  }
+};
+
+const display = {
+  template: "#slider-display",
+  components: {
+    thumbs,
+    buttons
+  },
+};
+
 new Vue ({
   el: "#slider-component",
-  template: "#works-slider"
+  template: "#works-slider",
+  components: {
+    display,
+    info
+  },
 });
